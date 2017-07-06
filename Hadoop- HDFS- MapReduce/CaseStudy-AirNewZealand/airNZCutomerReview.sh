83632a513cd8c0413343ff32d0e14a1cd90b6e3f
Akinwande Atanda <tripplehay777@gmail.com>
2017-07-07 10:31:04 +1200

    Bash shell script
    Command to run a MapReduce job using a single node HDFS cluster

---
+++ Hadoop- HDFS- MapReduce/CaseStudy-AirNewZealand/airNZCutomerReview.sh
@@ -0,0 +1,46 @@
+su - hduser
+mkdir -p localFS/data
+mv ANZFlightReviews.txt ./localFS/data/
+cd $HADOOP_HOME
+sbin/start-dfs.sh
+sbin/start-yarn.sh
+
+echo "Check the content of the distributed file system folder"
+bin/hdfs dfs -ls /user/hduser
+
+echo "Copy file from the local file system to the HDFS"
+bin/hdfs dfs -put /home/hduser/localFS/data /user/hduser
+
+echo "View the copied data"
+bin/hdfs dfs -ls /user/hduser/data
+
+
+echo "Run a MapReduce Job for counting words used in Air New Zealand Flight Review for April 2017"
+bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar wordcount /user/hduser/data /user/hduser/data-ANZOutput
+
+echo "View the output file"
+bin/hdfs dfs -ls /user/hduser/data-ANZOutput
+
+echo "Open the results within HDFS"
+bin/hdfs dfs -cat /user/hduser/data-ANZOutput/*0
+
+
+echo "Copy the results from HDFS to local FS directory"
+bin/hdfs dfs -get /user/hduser/data-ANZOutput /home/hduser/localFS/
+
+
+cd /home/hduser/localFS
+
+head ./data-ANZOutput/*0
+
+******************RESULTS***********************
+	302
+!!!	1
+"A"	1
+"Airpoints	2
+"MOVE	1
+"bleed	1
+"business	1
+"catering	1
+"closed"	1
+"economy	1
